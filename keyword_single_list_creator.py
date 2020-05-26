9eb77abcfdd874703d5275dd6f0529ba65cc8dd2
Michael Barrows <mail@michaelbarrows.co.uk>
2020-05-26 14:11:08 +0100

    commented file; also simplified how the list of original keywords is created.

--- keyword_single_list_creator.py
+++ keyword_single_list_creator.py
@@ -1,9 +1,17 @@
 import helpers
 import pandas as pd
 import dataset as ds
-from termcolor import colored
 
-def term_tagger (list_of_terms):
+# word_extractor()
+# parameters:
+#   list_of_terms : list - containing original keywords/keyterms
+# returns:
+#   terms_processed : list - a list containing the individual words of all of
+#       the keywords/phrases
+# description:
+#   This function extracts each individual word from a list of keywords/phrases.
+#   Duplicates are prevented and the list of individual words is returned
+def word_extractor(list_of_terms):
     terms_processed = []
     for items in list_of_terms:
         items = items.lower()
@@ -13,13 +21,21 @@ def term_tagger (list_of_terms):
                 terms_processed.append(item)
     return terms_processed
 
+# single_list_generator()
+# parameters:
+#   None
+# returns:
+#   output_file : string - path and filename for the file that the single list
+#       of keywords was stored in
+# description:
+#   This function takes a dataset of keywords/phrases, transforms the DataFrame
+#       to a list and sends the list to the word_extractor() function.
+#   The returned list of single keywords is transformed into a DataFrame and
+#       stored.
 def single_list_generator():
-    list_of_terms = []
     df = helpers.load_dataset(ds.output_data + "keywords/original_keywords.csv")
-    for index, row in df.iterrows():
-        list_of_terms.append(row.keywords)
-
-    tagged_terms = term_tagger(list_of_terms)
+    list_of_terms = df.keywords.tolist()
+    tagged_terms = word_extractor(list_of_terms)
     list_df = pd.DataFrame(tagged_terms, columns=["keyword"])
     output_dir = ds.output_data + "keywords/"
     helpers.path_checker(output_dir)
@@ -27,6 +43,15 @@ def single_list_generator():
     helpers.dataframe_to_csv(list_df, output_file)
     return output_file
 
+# run()
+# parameters:
+#   None
+# returns:
+#   None
+# description:
+#   This function is called to run the file (by calling the
+#       single_list_generator() function) and generates console output to inform
+#       the user of its purpose, progress and output files (with file paths).
 def run():
     print("\33[93m- keyword_single_list_creator.py\33[0m")
     print("  - Generating list of single keywords")
